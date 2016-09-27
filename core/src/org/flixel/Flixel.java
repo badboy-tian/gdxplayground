04ca40ccfc2953c10d19230b4c3fa2a911d8fd5e
tian <tqj.zyy@gmail.com>
2016-09-27 19:42:14 +0800

    增加橡皮擦效果

--- core/src/org/flixel/Flixel.java
+++ core/src/org/flixel/Flixel.java
@@ -1,33 +1,19 @@
 package org.flixel;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Flixel extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import org.flixel.screens.EarserTest;
+
+public class Flixel extends Game {
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create () {
+        setScreen(new EarserTest());
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
--- desktop/src/org/flixel/desktop/DesktopLauncher.java
+++ desktop/src/org/flixel/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import org.flixel.Flixel;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.width = 960;
+        config.height = 640;
+
 		new LwjglApplication(new Flixel(), config);
 	}
 }
