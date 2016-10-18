b9cab4677e74343f036f0ed52bd9c4af4f9a35a4
tian <tqj.zyy@gmail.com>
2016-10-18 10:00:33 +0800

    make the meshtools to java

---
+++ core/src/org/flixel/levelhelper/Utilities/LHScene.java
@@ -0,0 +1,8 @@
+package org.flixel.levelhelper.Utilities;
+
+/**
+ * Created by tian on 2016/10/18.
+ */
+
+public class LHScene {
+}
---
+++ core/src/org/flixel/levelhelper/Utilities/Node.java
@@ -0,0 +1,10 @@
+package org.flixel.levelhelper.Utilities;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by tian on 2016/10/18.
+ */
+
+public class Node extends Actor {
+}
